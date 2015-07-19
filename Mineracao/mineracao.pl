4a8e140dcb01c690e20fb16b0710b549f890c01d
igorminotto <igorcm92@hotmail.com>
2015-07-19 10:22:00 -0300

    Mudando exemplo em perl

--- Mineracao/mineracao.pl
+++ Mineracao/mineracao.pl
@@ -7,22 +7,29 @@ use File::Copy qw(copy);
 binmode(STDOUT, ":utf8");
 	
 my $query = 'Restaurante';
-my $url_base = "http://www.google.com/search?start=0&num=10&q=red&cr=countryCA&lr=lang_fr&client=google-csbe&output=xml_no_dtd&cx=00255077836266642015:u-scht7a-8i";
+my $url_base = "http://www.google.com/search?";
+my $dados = {
+  'start'=>0,
+  'num'=>10,
+  'q'=>"red+sox",
+	'cr'=>"countryCA",
+  'lr'=>"lang_fr",
+  'client'=>"google-csbe",
+  'output'=>"xml_no_dtd",
+  'cx'=>"00255077836266642015:u-scht7a-8i"
+};
+my @gets = ();
+while ( my ($key, $value) = each(%dados) ) {
+    push(@gets,"$key=$value");
+}
+$gets_str = join("&",@gets);
+my $url = $url_base.$gets_str;
 
 for (my $index=1; $index <= 1; $index++) {
-	if($index == 1) {
-		$url = $url_base."";
-	} else {
-		$url = $url_base."";
-	}
 	my $content = get $url;
 	die "Couldn't get $url" unless defined $content;
-	print $content;
-	my @matches = ($content =~ m/<div class="([A-Za-z-]*)"/g);
-	print "$index\n";
-	while(<@matches>) {
-		#print "\t$_\n";
-	}
+	open FILE, ">teste.html";
+	print FILE $content;
 }
 
 
