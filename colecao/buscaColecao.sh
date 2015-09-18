bc2fa8919327aaba6964a76f5c212a53b147d94d
igorminotto <igorcm92@hotmail.com>
2015-09-17 21:27:18 -0300

    Busca (com um parâmetro de cada vez) completa

--- colecao/buscaColecao.sh
+++ colecao/buscaColecao.sh
@@ -8,12 +8,39 @@ path=$path:../lucene-5.1.0/analysis/common/lucene-analyzers-common-5.1.0.jar
 
 pathIndex=./restaurantes/index
 
-if [ -z "$3" ]; then
-	lat='-23.5505199';
-	lon='-46.6333094';
-else
-	lat=$2;
-	lon=$3;
-fi
+query=$1;shift
+lat='-23.5505199';
+lon='-46.6333094';
+params='';
+while test $# -gt 0; do
+	case "$1" in
+    -lat)
+			shift
+			lat=$1
+			shift
+			;;
+		-lon)
+			shift
+			lon=$1
+			shift
+			;;
+		-distance)
+			params="$params $1"
+			shift
+			;;
+		-price)
+			params="$params $1"
+			shift
+			;;
+		-grade)
+			params="$params $1"
+			shift
+			;;
+		*)
+			echo "Argumento '$1' é inválido!"
+			exit
+			;;
+	esac
+done
 #java -classpath $path org.apache.lucene.demo.SearchFiles -query "$1" -paging 50 -index $pathIndex
-java -classpath $path core.SearchFiles -query "$1" -paging 50 -lat "$lat" -lng "$lon" -price
+java -classpath $path core.SearchFiles -query "$query" -paging 50 -lat "$lat" -lng "$lon" $params
