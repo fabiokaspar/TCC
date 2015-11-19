e653d2c03fa4eac43bcc607aacd56f8bd55b3ff3
igorminotto <igorcm92@hotmail.com>
2015-11-18 22:46:31 -0200

    Teste analytics

--- colecao/index.php
+++ colecao/index.php
@@ -82,6 +82,15 @@
             Criado por: Fábio Eduardo Kaspar, Igor Canko Minotto e Ricardo Oliveira Teles (2015)
         </div>
    </div>
-    <?php include_once("analyticstracking.php") ?>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-70372206-1', 'auto');
+        ga('send', 'pageview');
+
+      </script>
   </body>
 </html>
